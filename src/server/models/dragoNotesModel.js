5701fb235a36a85c3e5774edf041de6f00616e77
Sam Haar <samhaar@gmail.com>
2020-11-17 09:54:02 -0500

    initial set up of postgreSQL model

---
+++ src/server/models/dragoNotesModel.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+let PG_URI;
+if (process.env.NODE_ENV === 'test'){
+  PG_URI = process.env.POSTGRES_URI_TEST;
+}
+else if (process.env.NODE_ENV === 'dev'){
+  PG_URI = process.env.POSTGRES_URI_DEV;
+} else {
+  PG_URI = process.env.POSTGRES_URI_PROD;
+}
+
+const pool = new Pool({
+  connectionString: PG_URI,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
