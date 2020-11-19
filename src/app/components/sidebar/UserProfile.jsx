225f2f1070219f6bf9c7794a4708a731ad8c6097
Sam Carlile <samkcarlile@gmail.com>
2020-11-19 12:29:54 -0600

    more selectors, give them to me

--- src/app/components/sidebar/UserProfile.jsx
+++ src/app/components/sidebar/UserProfile.jsx
@@ -9,14 +9,23 @@ import {
   MenuList,
   MenuItem,
 } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
 
 export default function UserProfile() {
+  const { avatarUrl, name } = useSelector((state) => state.user);
+
   return (
     <Flex h="60px" direction="row" alignItems="center" m={5}>
-      <Avatar name="UserPicture" src="" size="md" boxShadow="lg" mx={5} />
+      <Avatar
+        name="UserPicture"
+        src={avatarUrl}
+        size="md"
+        boxShadow="lg"
+        mx={5}
+      />
       <Menu>
         <MenuButton as={Button} colorScheme="gray" variant="ghost">
-          Username
+          {name}
         </MenuButton>
         <MenuList>
           <MenuItem>Sign out</MenuItem>
